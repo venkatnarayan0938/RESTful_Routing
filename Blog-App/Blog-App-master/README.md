1707625b1ff779efee7b04b633a3d27dc98584fe
venkatnarayan0938 <60142552+venkatnarayan0938@users.noreply.github.com>
2020-10-27 15:59:00 +0530

    Add files via upload

---
+++ Blog-App/Blog-App-master/README.md
@@ -0,0 +1,22 @@
+# RESTful Blog App
+https://glacial-fjord-10639.herokuapp.com/
+
+RESTful Blog App is an application that uses Express.js, Node.js, and MongoDB to allow its user to create, read, update, and delete (CRUD) various blog posts. 
+
+### How It Works
+* The user can simply view blog posts on the landing page.
+* The user can also create a new post using the link in the navigation bar.
+* Users can edit and delete each post once they click "Read More" on the desired post.
+
+### Technologies Used
+* JavaScript
+* Semantic UI
+* REST
+* MongoDB
+* Mongoose
+* HTML
+* CSS
+* EJS
+* Express.js
+* Node.js
+
---
+++ Blog-App/Blog-App-master/app.js
@@ -0,0 +1,108 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var methodOverride = require("method-override");
+var expressSanitizer = require("express-sanitizer");
+
+// App config
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/restful_blog_app";
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
+var PORT = process.env.PORT || 3001;
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
+
+// Mongoose/Model config
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+// RESTful routes
+app.get("/", function(req, res) {
+    res.redirect("/blogs");
+})
+
+// Index route
+app.get("/blogs", function(req, res) {
+    Blog.find({}, function(err, blogs) {
+        if(err) {
+            console.log(err)
+        } else {
+            res.render("index", {blogs: blogs});
+        }
+    });
+});
+
+// New route
+app.get("/blogs/new", function(req, res) {
+    res.render("new");
+});
+
+// Create route
+app.post("/blogs", function(req, res) {
+    req.body.blog.body = req.sanitize(req.body.blog.body)
+    Blog.create(req.body.blog, function(err, newBlog) {
+        if(err) {
+            res.render("new");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+});
+
+// Show route
+app.get("/blogs/:id", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if(err) {
+            res.redirect("/blogs");
+        } else {
+            res.render("show", {blog: foundBlog});
+        }
+    });
+});
+
+// Edit route
+app.get("/blogs/:id/edit", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if(err) {
+            res.redirect("/blogs")
+        } else {
+            res.render("edit", {blog: foundBlog})
+        }
+    });  
+});
+
+// Update route
+app.put("/blogs/:id", function(req, res) {
+    req.body.blog.body = req.sanitize(req.body.blog.body)
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
+        if(err) {
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
+});
+
+// Destroy route
+app.delete("/blogs/:id", function(req, res) {
+    Blog.findByIdAndRemove(req.params.id, function(err) {
+        if(err) {
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+});
+
+app.listen(PORT, function() {
+    console.log("App is listening on port " + PORT + "!");
+});
